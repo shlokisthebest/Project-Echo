8abe76bcd0c477d1f65360c61fabbb2aed5cd121
Shlok Gupta <shlokisthebest@gmail.com>
2019-03-10 03:02:18 +0530

    removing un required code

--- src/app/echo-indoor/all-product-display/filter-products/filter-products.component.ts
+++ src/app/echo-indoor/all-product-display/filter-products/filter-products.component.ts
@@ -10,17 +10,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FilterProductsComponent implements OnInit {
   public filterd:any;
-  // public prods:ProductType[];
-  // public temp:ProductType[];
-
   category:string;
   type:string;
-  data1:any;
   data2:any;
-
   constructor(private productService:IndoorproductsService,private route:ActivatedRoute) {
-    
-    
   }
 
   ngOnInit() {
@@ -28,10 +21,8 @@ export class FilterProductsComponent implements OnInit {
     .subscribe(params =>{
       this.category = params.get('category');
       this.type = params.get('type');
-      //console.log(id);
       console.log("category:"+this.category);
       console.log("type :"+this.type);
-      // this.fireEvent(this.category,this.type);
       this.productService.getProducts(this.category,this.type).subscribe(dataq =>{
         this.filterd = dataq;
         console.log(dataq);
@@ -43,31 +34,20 @@ export class FilterProductsComponent implements OnInit {
 
   fireEvent(filtered,category,type)
   {
-    // console.log(this.data1);
       if(category === null)
       {
-        console.log('hello');
-          // console.log(this.data1)
           return filtered
       }
       else if(type === null)
       {
-        console.log(category);
-        console.log(filtered);
         filtered = filtered.filter(prod => prod.Type == category);
-        console.log(filtered);
         return filtered;
       }
       else
       {
-        console.log('peopl');
         this.data2 = filtered.filter(prod => prod.Type === category);
-        console.log(this.data2);
-        console.log(type);
         let data3= '';
         data3 = this.data2[0].Products.filter(p => p.type === type);
-        console.log(data3);
-        
         this.data2[0].Products = data3;
         console.log(this.data2);
         return this.data2;
--- src/app/service/indoorproducts.service.ts
+++ src/app/service/indoorproducts.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-
-import { ProductType,ProductModel,Spec } from '../modal/Product';
-import { PRODS } from '../modal/productList';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,11 +7,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class IndoorproductsService {
-  
-  data1:any;
-  data2:any;
-  public out:any;
-
   constructor(private http : HttpClient) { }
 
   public getProducts(category,type):Observable<any>{
